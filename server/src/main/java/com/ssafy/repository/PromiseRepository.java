d687afb22ba468987193bcede3dd97bf0b7eb8b0
hongzzi <hongzzi.dev@gmail.com>
2020-02-05 18:03:03 +0900

    add home

--- server/src/main/java/com/ssafy/repository/PromiseRepository.java
+++ server/src/main/java/com/ssafy/repository/PromiseRepository.java
@@ -14,5 +14,10 @@ public interface PromiseRepository extends JpaRepository<Promise, Long> {
 	
 	@Query(value = "select * from promise pr where pr.p_id=?1", nativeQuery = true)
 	List<Promise>  findByPoliticianId(int pId );
+
+	@Query(value = "select * from promise where P_ID IN ( select * from ((select a.P_ID from promise as a left join politician as b on a.P_ID = b.P_ID where P_PARTY = \"자유한국당\" order by rand() limit 2) as temp ))", nativeQuery = true)
+	List<Promise>  findByPromise(int pId );
+	//이름 수정
 	
 }
+
