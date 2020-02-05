dbd9d4a01eeeced0d53aba0afbe058cb021da6ed
김은수 <dmstndlekd@gmail.com>
2020-02-05 17:42:11 +0900

    200205|React|writeActionButton_ui

--- client/src/blog/Posts/Board/pages/WritePage.js
+++ client/src/blog/Posts/Board/pages/WritePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Responsive } from 'semantic-ui-react';
 import Editor from '../write/Editor';
+import WriteActionButtons from '../write/WriteActionButtons';
 
 const WritePage = () => {
     return (
         <Responsive>
             <Editor />
+            <WriteActionButtons />
         </Responsive>
     );
 };
