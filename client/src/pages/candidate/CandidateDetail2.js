5089e8ef2fd8b430030e7089ed4d0bc8741173f3
김은수 <dmstndlekd@gmail.com>
2020-02-13 10:38:34 +0900

    200213|React|Candidate_pdf

--- client/src/pages/candidate/CandidateDetail2.js
+++ client/src/pages/candidate/CandidateDetail2.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Document, Page } from "react-pdf/dist/entry.webpack";
-import pdfFile from "./pdf/gangnam_gab_2.pdf";
+import pdfFile from "./pdf/Candidate_2.pdf";
 
 class CandidateCardDetail extends Component {
   state = {
