7f2d5216d46d84658b34bf813d1d49e2c06e0099
heeseonim <hisundl@naver.com>
2020-02-18 14:38:58 +0900

    대통령선거

--- client/src/pages/main/MainPage.js
+++ client/src/pages/main/MainPage.js
@@ -7,7 +7,7 @@ import FeaturedPost from './MainMenu';
 import { Link } from 'react-router-dom';
 
 const mainFeaturedPost = {
-  title: '우리동네',
+  title: '제19대 대통령 선거',
   title2: '후보자 살펴보기',
   description: "지금 바로 살펴보세요!",
   image: 'https://trulybelong.com/wp-content/uploads/2019/11/SOC-voting-image-TEXTLESS-1280x640.jpg',
